c6e5c6e090a26418d9141aa549deb260b8058a03
Arun P Johny <arun.official.mail@gmail.com>
2010-09-27 22:18:53 +0530

    Organize imports

--- src/java/com/arun/controller/TestController.java
+++ src/java/com/arun/controller/TestController.java
@@ -2,7 +2,6 @@ package com.arun.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
